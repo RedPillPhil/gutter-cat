bafa30f25511bdfac8b4d1cdb7f6ecc147351869
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-21 01:25:15 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        coiny: ['Coiny', ...defaultTheme.fontFamily.sans]
-      },
-      colors: {
-        'brand-purple': 'var(--clr-purple)',
-        'brand-pink': 'var(--clr-pink)',
-        'brand-yellow': 'var(--clr-yellow)',
-        'brand-blue': 'var(--clr-blue)',
-        'brand-green': 'var(--clr-green)',
-        'brand-light': 'var(--clr-light)',
-        'brand-background': 'var(--clr-background)'
-      },
-      animation: {
-        'pulse-slow': 'pulse 10s linear infinite'
-      }
-    }
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
