bca6408a27f617fdc9d9155f3053df9bb85b2c48
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-21 03:15:12 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,29 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-var textBlock = document.createElement('div');
-textBlock.contentEditable = true;
-textBlock.style.border = '1px solid black';
-textBlock.style.padding = '10px';
-textBlock.style.margin = '10px';
-document.body.appendChild(textBlock);
-/* ReactDOM.createRoot(document.getElementById('root')).render( */
-var reactRoot = document.createElement('div');
-reactRoot.id = 'root';
-document.body.appendChild(reactRoot);
-/* <React.StrictMode> */
-var reactStrictMode = document.createElement('div');
-reactStrictMode.innerHTML = '<React.StrictMode>';
-reactRoot.appendChild(reactStrictMode);
-/* <App /> */
-var reactApp = document.createElement('div');
-reactApp.innerHTML = '<App />';
-reactStrictMode.appendChild(reactApp);
-/* </React.StrictMode> */
-var reactStrictModeEnd = document.createElement('div');
-reactStrictModeEnd.innerHTML = '</React.StrictMode>';
-reactRoot.appendChild(reactStrictModeEnd);
-/* ) */
-var reactRootEnd = document.createElement('div');
-reactRootEnd.innerHTML = ')';
-document.body.appendChild(reactRootEnd);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
