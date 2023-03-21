9a295c79435c95bd39b37590be642573d4464e6c
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-21 03:12:42 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+/* Create a block inside this code snippet that I can write some text in */
+var textBlock = document.createElement('div');
+textBlock.contentEditable = true;
+textBlock.style.border = '1px solid black';
+textBlock.style.padding = '10px';
+textBlock.style.margin = '10px';
+document.body.appendChild(textBlock);
+/* ReactDOM.createRoot(document.getElementById('root')).render( */
+var reactRoot = document.createElement('div');
+reactRoot.id = 'root';
+document.body.appendChild(reactRoot);
+/* <React.StrictMode> */
+var reactStrictMode = document.createElement('div');
+reactStrictMode.innerHTML = '<React.StrictMode>';
+reactRoot.appendChild(reactStrictMode);
+/* <App /> */
+var reactApp = document.createElement('div');
+reactApp.innerHTML = '<App />';
+reactStrictMode.appendChild(reactApp);
+/* </React.StrictMode> */
+var reactStrictModeEnd = document.createElement('div');
+reactStrictModeEnd.innerHTML = '</React.StrictMode>';
+reactRoot.appendChild(reactStrictModeEnd);
+/* ) */
 )
